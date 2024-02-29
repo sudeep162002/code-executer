321593fa09454867c0764ff2656a42ac85e5a371
sudeep162002 <80563363+sudeep162002@users.noreply.github.com>
2024-02-29 22:45:23 +0530

    add readfile sync

--- languge-container/index.ts
+++ languge-container/index.ts
@@ -1,11 +1,20 @@
 import * as Kafka from 'node-rdkafka';
-
+import * as fs from 'fs';
 const consumer = new Kafka.KafkaConsumer({
   'group.id': 'kafka',
   'metadata.broker.list': 'localhost:9092',
 }, {});
 
 consumer.connect();
+// const inputString = "#include <iostream> int main() { std::cout << "Hello, World!" << std::endl; return 0; }";
+const filePath = "./input.cpp";
+
+function copyStringToCppFile(inputString, filePath) {
+  // Append the input string to the cpp file
+  fs.appendFileSync(filePath, inputString + '\n'); // Add newline for clarity
+  console.log(`String appended to ${filePath}`);
+}
+// copyStringToCppFile(inputString,filePath);
 
 consumer.on('ready', () => {
   console.log('consumer ready..');
@@ -15,4 +24,5 @@ consumer.on('ready', () => {
   console.log(data);
   const stringValue: string = data.value?.toString('utf8') || '';
   console.log(`received message: ${stringValue}`);
+  copyStringToCppFile(stringValue,filePath);
 });
